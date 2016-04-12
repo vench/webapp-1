f617a97ec68b6b6d2f5d8cf5201c0bac397c50b6
Grigory Kislin <javawebinar@yandex.ru>
2016-04-12 22:59:09 +0300

    Add logging Add MainStream

--- src/ru/javawebinar/webapp/storage/AbstractStorage.java
+++ src/ru/javawebinar/webapp/storage/AbstractStorage.java
@@ -7,5 +7,15 @@ import java.util.logging.Logger;
  * 08.04.2016
  */
 abstract public class AbstractStorage implements Storage {
+//    private final static Logger LOG = Logger.getLogger(AbstractStorage.class.getName());
+
     protected final Logger log = Logger.getLogger(getClass().getName());
+
+    @Override
+    public void clear() {
+        log.info("clear");
+        doClear();
+    }
+
+    protected abstract void doClear();
 }
