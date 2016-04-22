cbb515089efcd311f87f986b8a278b50e3c01bdf
Grigory Kislin <javawebinar@yandex.ru>
2016-04-22 23:02:35 +0300

    Add request encoding

--- src/ru/javawebinar/webapp/web/ResumeServlet.java
+++ src/ru/javawebinar/webapp/web/ResumeServlet.java
@@ -13,6 +13,7 @@ import java.io.IOException;
 public class ResumeServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
         String name = req.getParameter("name");
         resp.getWriter().write("Hello " + (name == null ? "WebApp" : name) + "!");
     }
