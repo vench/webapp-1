b930311aea83f7cd2a72d8107f8526d225165f4d
Grigory Kislin <javawebinar@yandex.ru>
2016-04-22 22:49:55 +0300

    Add ResumeServlet

--- src/ru/javawebinar/webapp/web/ResumeServlet.java
+++ src/ru/javawebinar/webapp/web/ResumeServlet.java
@@ -1,8 +1,18 @@
 package ru.javawebinar.webapp.web;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  * GKislin
  * 22.04.2016
  */
-public class ResumeServlet extends javax.servlet.http.HttpServlet {
+public class ResumeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("Hello Webapp!");
+    }
 }
